d5185c2e39225b7c84ce684c2769a0cbdd206c2e
David Zhu <hdavidzhu@gmail.com>
2016-01-19 18:49:09 -0500

    ADD: Comments for index.js

--- demo/index.js
+++ demo/index.js
@@ -1,3 +1,5 @@
+var $errors = $("#errors");
+
 var myCodeMirror = CodeMirror(document.getElementById("code-block"), {
   value: "var x = 4;",
   mode: "javascript",
@@ -5,25 +7,30 @@ var myCodeMirror = CodeMirror(document.getElementById("code-block"), {
 });
 
 function lint() {
-  document.getElementById("errors").innerText = "";
+  // First, clear the error box.
+  $errors.empty();
 
+  // Then, grab our code text.
   var code  = myCodeMirror.getValue();
 
   try {
+    // Convert code and detect errors.
     var esprimadCode = esprima.parse(code, { tolerant: true });
     var errors = esprimadCode.errors;
-    $("#errors").append(errors.length);
+    $errors.append(errors.length);
 
+    // Lint the code and emit any errors.
     KALint.lint(esprimadCode, myExport.whiteListCond_1, function(error) {
       // This callback gets called whenever an error is detected in runtime.
-      $("#errors").append(error.error);
+      $errors.append(error.error);
     });
   } catch(e) {
-    $("#errors").append(e.toString());
+    $errors.append(e.toString());
   }
 }
 
 // From: https://davidwalsh.name/javascript-debounce-function
+// Prevents the linter from constantly running.
 function debounce(func, wait, immediate) {
 	var timeout;
 	return function() {
@@ -39,7 +46,7 @@ function debounce(func, wait, immediate) {
 	};
 }
 
-var debouncedLogCode = debounce(logCode, 250);
-
+// Run.
 logCode();
+var debouncedLogCode = debounce(logCode, 250);
 $("#code-block").on("input", debouncedLogCode);
